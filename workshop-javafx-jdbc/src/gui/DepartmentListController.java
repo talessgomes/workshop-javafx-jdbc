4ed7c27dd9d9a14c026e97171dcb687301fbbef1
talessgomes <tales.gomes_sl@hotmail.com>
2022-05-09 16:00:06 -0300

    initializing-action-as-parameter

--- workshop-javafx-jdbc/src/gui/DepartmentListController.java
+++ workshop-javafx-jdbc/src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,36 +15,54 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
-	
+
+	private DepartmentService service;
+
 	@FXML
 	private TableView<Department> tableViewDepartment;
 
 	@FXML
 	private TableColumn<Department, Integer> tableColumnId;
-	
+
 	@FXML
 	private TableColumn<Department, String> tableColumnName;
-	
+
 	@FXML
 	private Button btNew;
- 	
+
+	private ObservableList<Department> obslist;
+
 	@FXML
 	public void onBtNewAction() {
 		System.out.println("onBtNewAction");
 	}
-	
-	public void initialize(URL url, ResourceBundle rb) {	
+
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
+
+	public void initialize(URL url, ResourceBundle rb) {
 		initizalizeNodes();
 	}
 
 	private void initizalizeNodes() {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
-		
+
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 	}
+
+	public void updateTableView() {
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		List<Department> list = service.findAll();
+		obslist = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obslist);
+				}
 
 }
