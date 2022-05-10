ff8b55b4144893004606d2523df432c74f9c6793
talessgomes <tales.gomes_sl@hotmail.com>
2022-05-10 11:31:56 -0300

    saving-a-new-department

--- workshop-javafx-jdbc/src/gui/DepartmentListController.java
+++ workshop-javafx-jdbc/src/gui/DepartmentListController.java
@@ -83,6 +83,7 @@ public class DepartmentListController implements Initializable {
 			
 			DepartmentFormController controller = loader.getController();
 			controller.setDeparment(obj);
+			controller.setDepartmentService(new DepartmentService());
 			controller.updateFormData();
 			
 			Stage dialogStage = new Stage();
